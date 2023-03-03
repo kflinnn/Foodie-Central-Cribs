8b8ec62f7d229b995a6da09b0435f6e57cbff24a
GeckoGiggles <eva.judith.perez@gmail.com>
2023-03-02 19:15:44 -0500

    comment model, routes, and index all adjusted to include comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipeRoutes = require('./recipeRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/', userRoutes);
 router.use('/', recipeRoutes);
+router.use('/', commentRoutes);
 
 module.exports = router;
