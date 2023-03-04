bfcf1ce4d9d66c1f4b9643ddb6732ef8d1bdc7f1
kflinnn <kflinncoding@gmail.com>
2023-03-04 12:38:01 -0500

    updated js logic, routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipeRoutes = require('./recipeRoutes');
-const commentRoutes = require('./commentRoutes');
 
-router.use('/', userRoutes);
-router.use('/', recipeRoutes);
-router.use('/', commentRoutes);
+//Future development - Comment Section
+// const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/recipes', recipeRoutes);
+
+//Future development - Comment Section
+// router.use('/', commentRoutes);
 
 module.exports = router;
