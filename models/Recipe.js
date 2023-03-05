2402f0dedfc722e7a58af35ee2a6a40006da7b9b
kflinnn <kflinncoding@gmail.com>
2023-03-05 16:38:42 -0500

    fixed routes, fixed handlebars, fixed partials, all routes working

--- models/Recipe.js
+++ models/Recipe.js
@@ -20,7 +20,7 @@ Recipe.init(
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     date_created: {
       type: DataTypes.DATE,
--- public/js/login.js
+++ public/js/login.js
@@ -2,12 +2,12 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const username = document.querySelector('#login-username').value.trim();
-  const password = document.querySelector('#login-password').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -44,9 +44,9 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector('#login-btn')
   .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector('.signup-form')
+  .querySelector('#signup-btn')
   .addEventListener('submit', signupFormHandler);
