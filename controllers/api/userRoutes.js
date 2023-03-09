d521fd5d813a4a2bf9aa03cff5162a6e8c1c6615
kflinnn <kflinncoding@gmail.com>
2023-03-08 23:32:20 -0500

    debugged logout button and homepage button

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/user/logout', (req, res) => {
+router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,9 +1,9 @@
-const CreateRecipe = async (event) => {
+const createRecipe = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
     const title = document.querySelector('#title').value.trim();
-    const userName = document.querySelector('#username').value.trim();
+    const user_name = document.querySelector('#username').value.trim();
     const description = document.querySelector('#Description').value.trim();
     const ingredients = document.querySelector('#Ingredients').value.trim();
     const instructions = document.querySelector('#Instructions').value.trim();
@@ -11,11 +11,11 @@ const CreateRecipe = async (event) => {
 
 
   
-    if (title && userName && description && ingredients && instructions) {
+    if (title && user_name && description && ingredients && instructions) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/recipes', {
         method: 'POST',
-        body: JSON.stringify({ title, userName, description, ingredients, instructions}),
+        body: JSON.stringify({ title, user_name, description, ingredients, instructions}),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -28,10 +28,31 @@ const CreateRecipe = async (event) => {
     }
   };
   
+//function to view selected recipe
+const selectRecipe = async () => {
+//needs way to collect recipe id
+
+  console.log("You've selected a recipe!");
+  const response = await fetch('/api/recipes/:id', {
+    method: 'POST',
+    headers: { 'Content-Type' : 'application/json'},
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
 
   //need to check to make sure that is the correct query selector for the button
   document
     .querySelector('.login-form')
-    .addEventListener('click', CreateRecipe);
+    .addEventListener('click', createRecipe);
 
     //Potentially need a function for a button for a user to save any recipe to their kitchen
+
+  document
+    .querySelector('.shelf-con')
+    .addEventListener('click', selectRecipe);
