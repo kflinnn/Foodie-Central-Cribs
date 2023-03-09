2b8288f8f1c9c4b47fba310826c8a9f2408af2b7
GeckoGiggles <eva.judith.perez@gmail.com>
2023-03-09 16:01:08 -0500

    fixed styling for the buttons on the profile page.

--- public/js/selectRecipe.js
+++ public/js/selectRecipe.js
@@ -1,23 +1,32 @@
+
 //function to view selected recipe - this does not work
-const selectRecipe = async () => {
-    //needs way to collect recipe id
-      console.log("You've selected a recipe!");
-      const recipeTitle = document.querySelector('#select-recipe').value;
-      console.log(recipeTitle);
-    
-      const response = await fetch('/recipe/:id', {
-        method: 'GET',
-        headers: { 'Content-Type' : 'application/json'},
-      });
-    
-      if (response.ok) {
-        document.location.replace('/recipe/:id');
-      } else {
-        alert(response.statusText);
-      }
-    };
+const selectRecipe = async (recipeDetails) => {
+  //needs way to collect recipe id
+  console.log("You've selected a recipe!");
+  const recipeTitle = document.querySelector('#select-recipe');
+  console.log(recipeTitle);
+  try {
+    const recipeData = await Recipe.findByPk(req.params.id);
+    res.status(200).json(recipeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  const response = await fetch(`/recipe/${req.body}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+
+  });
+
+  if (response.ok) {
+    console.log(response)
+    // console.log(body.id)
+    document.location.replace(`/recipe/${body.id}`);
+  } else {
+    alert(response.statusText);
+  }
+};
 
 //Button to view recipe details
-  document
-    .getElementById('select-recipe')
-    .addEventListener('click', selectRecipe);
+document
+  .getElementById('select-recipe')
+  .addEventListener('click', selectRecipe);
